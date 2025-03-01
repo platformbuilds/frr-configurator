4766afb2fc6ba9eccb7ab82e1fbb83929d77c2eb
aarvee11 <shiva.rv@npci.org.in>
2025-03-01 13:28:48 +0530

    get all svc

--- config/kubernetes/get_svc_ips.py
+++ config/kubernetes/get_svc_ips.py
@@ -9,24 +9,25 @@ logging.basicConfig(
     datefmt='%Y-%m-%dT%H:%M:%S.000Z',
     )
 
-if os.path.isfile("/root/.kube/config"):
-    config.load_kube_config()
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    
-    svc_listing = v1.list_service_for_all_namespaces(watch=False)
-    
-    for svc in svc_listing.items:
-        svc_name = svc.metadata.name
-        svc_namespace = svc.metadata.namespace
-        svc_cluster_ips = svc.spec.cluster_i_ps
-        svc_data = {
-            "service_name": svc_name,
-            "service_namespace": svc_namespace,
-            "service_cluster_ips": svc_cluster_ips
-        }
-        print(svc_data)
+def get_kube_svc():
+    svc_list = []
+    if os.path.isfile("/root/.kube/config"):
+        config.load_kube_config()
+        v1 = client.CoreV1Api()
+        logging.info("Listing running services")
+        svc_listing = v1.list_service_for_all_namespaces(watch=False)
+        for svc in svc_listing.items:
+            svc_name = svc.metadata.name
+            svc_namespace = svc.metadata.namespace
+            svc_cluster_ips = svc.spec.cluster_i_ps
+            svc_data = {
+                "service_name": svc_name,
+                "service_namespace": svc_namespace,
+                "service_cluster_ips": svc_cluster_ips
+            }
+            svc_list.append(svc_data)
 
-else:
-    logging.error("Kubeconfig not found !")
-    quit()
+    else:
+        logging.error("Kubeconfig not found !")
+
+    return svc_list
