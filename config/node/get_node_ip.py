c97edf363fa8292cd5088da8c749a5137e0734e6
aarvee11 <shiva.rv@npci.org.in>
2025-03-01 20:31:47 +0530

    get all ing

--- config/node/get_node_ip.py
+++ config/node/get_node_ip.py
@@ -4,6 +4,6 @@ loopback_int_data = netifaces.ifaddresses("lo")
 
 for int_index in loopback_int_data:
     for addresses in loopback_int_data[int_index]:
-            for address_detail in addresses:
-                    if address_detail["netmask"] == "255.255.255.255":
-                            print(address_detail)
+            for address_details in addresses:
+                    for address in address_details:
+                        print(address)
