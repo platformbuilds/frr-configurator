dcc17752f67ea3b6e8c8ed1e29ae33e1417e4795
aarvee11 <shiva.rv@npci.org.in>
2025-03-01 13:51:38 +0530

    get all svc

--- config/kubernetes/liveapps.py
+++ config/kubernetes/liveapps.py
@@ -8,13 +8,21 @@ logging.basicConfig(
     datefmt='%Y-%m-%dT%H:%M:%S.000Z',
     )
 
+if os.path.isfile("/root/.kube/config"):
+    config.load_kube_config()
+    coreapiv1 = client.CoreV1Api()
+    ingressapiv1 = client.V1Ingress()
+    logging.info("successfully loaded kubeconfig")
+else:
+    logging.error("kubeconfig not found. ensure node is a valid kube node")
+
+
+
 def get_kube_svc():
     svc_list = []
-    if os.path.isfile("/root/.kube/config"):
-        config.load_kube_config()
-        v1 = client.CoreV1Api()
+    try:
         logging.info("Listing running services")
-        svc_listing = v1.list_service_for_all_namespaces(watch=False)
+        svc_listing = coreapiv1.list_service_for_all_namespaces(watch=False)
         for svc in svc_listing.items:
             svc_name = svc.metadata.name
             svc_namespace = svc.metadata.namespace
@@ -26,7 +34,17 @@ def get_kube_svc():
             }
             svc_list.append(svc_data)
 
-    else:
-        logging.error("Kubeconfig not found !")
-
+    except Exception as e:
+        logging.exception(e)
     return svc_list
+
+def get_my_ingress_pod_ip():
+    node_ingress_pod = []
+    try:
+        logging.info("Listing ingress pods")
+        ingress_pod = ingressapiv1.status()
+        node_ingress_pod.append(ingress_pod)
+    except Exception as e:
+        logging.exception(e)
+
+    return node_ingress_pod
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import json
-from config.kubernetes.get_svc_ips import get_kube_svc
+from config.kubernetes.liveapps import get_kube_svc
 
 
-print(json.dumps(get_kube_svc()))
+# fetch the running services and ingress pod IPs 
+services = get_kube_svc()
+ingress = get_my_ingress_pod_ip()
+
+# Check if the current svc can be handled by the node - self check
+'''
+Check the 
+'''
+
+# build the vtysh config
+
