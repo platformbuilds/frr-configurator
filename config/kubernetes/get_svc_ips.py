d460909bc8f9e89da568a6a3d43b4ec9c7b32e56
aarvee11 <shiva.rv@npci.org.in>
2025-03-01 13:25:20 +0530

    get all svc

--- config/kubernetes/get_svc_ips.py
+++ config/kubernetes/get_svc_ips.py
@@ -13,11 +13,19 @@ if os.path.isfile("/root/.kube/config"):
     config.load_kube_config()
     v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
-    ret = v1.list_service_for_all_namespaces(watch=False)
-    for svc in ret.items:
-        svc_metadata = svc.metadata
-        print(svc_metadata)
-        print(type(svc_metadata))
+    
+    svc_listing = v1.list_service_for_all_namespaces(watch=False)
+    
+    for svc in svc_listing.items:
+        svc_name = svc.metadata.name
+        svc_namespace = svc.metadata.namespace
+        svc_cluster_ips = svc.spec.cluster_i_ps
+        svc_data = {
+            "service_name": svc_name,
+            "service_namespace": svc_namespace,
+            "service_cluster_ips": svc_cluster_ips
+        }
+        print(svc_data)
 
 else:
     logging.error("Kubeconfig not found !")
