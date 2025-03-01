85308a94cbbf4ada397f26515dd5af3d107117fb
aarvee11 <shiva.rv@npci.org.in>
2025-03-01 20:05:25 +0530

    get all ing

--- config/kubernetes/liveapps.py
+++ config/kubernetes/liveapps.py
@@ -39,7 +39,9 @@ def get_kube_ingress_pods(ingress_namespace):
     ingress_pod_ips = []
     try:
         coreapiv1 = client.CoreV1Api()
-        ingress_pod_ips = coreapiv1.list_namespaced_pod(namespace=ingress_namespace).items["status"]["pod_i_ps"]
+        ingress_pods = coreapiv1.list_namespaced_pod(namespace=ingress_namespace).items
+        for ingress_pod_ip in ingress_pods:
+            ingress_pod_ips.append(ingress_pod_ip["status"]["pod_i_ps"])
     except Exception as e:
         logging.exception(e)
     return ingress_pod_ips
