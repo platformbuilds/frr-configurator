1365e5259c9e268f6753aa2e376678061b234a75
aarvee11 <shiva.rv@npci.org.in>
2025-03-02 19:29:03 +0530

    get all ing

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import json
 from config.kube.liveapps import get_kube_svc, get_my_ingress_pod_ip, get_kube_app_with_ingress_all
 
+'''
+check if node is ingress
+list all the configured ingress apps
+check if app on ingress is configurable
+    yes: bring up the ClusterIP on FRR up
+    no: bring down the ClusterIP on FRR down
+loop every second !
+'''
+
+
 
 # fetch the running services and ingress pod IPs 
 services = get_kube_svc()
 node_ingress_details = get_my_ingress_pod_ip()
 app_ingresses = get_kube_app_with_ingress_all()
 
-print(services)
-print(node_ingress_details)
-print(app_ingresses)
-
-'''
-# Check if the current svc can be handled by the node - self check
-Check the status of the ingress pod on the current node
-if present, check its health for the service being added
-and if all good, then create a list of service ips to be added to FRR loopback interfaces
-'''
-
-# build the vtysh config
-
+print(json.dumps(services))
+print(json.dumps(node_ingress_details))
+print(json.dumps(app_ingresses))
