4ea5ca1b44a672b4a6e23d95e10a1c9ee5b7c722
aarvee11 <shiva.rv@npci.org.in>
2025-03-03 11:02:30 +0530

    get all ing

--- main.py
+++ main.py
@@ -33,6 +33,10 @@ l7_ingress_services_to_expose = []
 for app_ingress in app_ingresses:
     l7_ingress_services_to_expose.append(app_ingress["name"])
 
+node_ingresses = []
+for ingress in node_ingress_details:
+    node_ingresses.append(ingress["ingress_release_name"])
+
 # Check the health of each service and then expose the ClusterIP if the node is able to handle the traffic
 l4_kube_proxy_services_to_expose = []
 for svc in services:
@@ -40,9 +44,16 @@ for svc in services:
         l4_kube_proxy_services_to_expose.append(svc["service_name"])
 
 
-print("{}\n".format(json.dumps(services)))
-print("{}\n".format(json.dumps(app_ingresses)))
-print("{}\n".format(json.dumps(node_kube_proxy_details)))
-print("{}\n".format(json.dumps(node_ingress_details)))
-print("{}\n".format(json.dumps(l7_ingress_services_to_expose)))
-print("{}\n".format(json.dumps(l4_kube_proxy_services_to_expose)))
+#print("{}\n".format(json.dumps(services)))
+
+print("L7 Ingresses to be BGP Advertised:\n{}".format(json.dumps(node_ingresses)))
+print("L7 Applications that are automatically getting advertised:\n{}".format(json.dumps(app_ingresses)))
+
+print("L4 Kube-Proxy ClusterIP SVCs to be BGP Advertised:\n{}".format(json.dumps(l4_kube_proxy_services_to_expose)))
+
+
+#print("{}\n".format(json.dumps(app_ingresses)))
+#print("{}\n".format(json.dumps(node_kube_proxy_details)))
+#print("{}\n".format(json.dumps(node_ingress_details)))
+#print("{}\n".format(json.dumps(l7_ingress_services_to_expose)))
+#print("{}\n".format(json.dumps(l4_kube_proxy_services_to_expose)))
