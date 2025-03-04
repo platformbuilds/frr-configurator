978838d0b825c67fa55597711cd0d48e30eea8bf
anuragkhuntia <kanurag03@gmail.com>
2025-03-04 12:17:27 +0530

    added frr module to advertise loopback addr

--- helpers/config/vrouter/frr/router.py
+++ helpers/config/vrouter/frr/router.py
@@ -1,4 +1,45 @@
 import os
 import sys
+import subprocess
 my_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, my_path + '/../../../../')
+
+
+def update_loopback_subprocess(ip, as_no):
+    commands = f"""
+    configure terminal
+    interface lo
+    ip address {ip}/32
+    exit
+    router bgp {as_no}
+    address-family ipv4 unicast
+    network {ip}/32
+    end
+    write memory
+    end
+    """
+
+    try:
+        process = subprocess.run(["vtysh"],input=commands, text=True, capture_output=True)
+
+        if process.returncode == 0:
+            print("FRR config updated successfully")
+        else:
+            print("error updating FRR conf:", process.stderr)
+    except Exception as e:
+        print("Exception occured", str(e))
+
+def update_loopback_os(ip, as_no):
+    commands = (
+        f'vtysh -c "configure terminal" '
+        f'-c "interface lo" '
+        f'-c "ip address {ip}/32" '
+        f'-c "exit" '
+        f'-c "router bgp {as_no}" '
+        f'-c "address-family ipv4 unicast" '
+        f'-c "network {ip}/32" '
+        f'-c "end" '
+        f'-c "write memory" '
+        f'-c "end" '
+    )
+    os.system(commands)
